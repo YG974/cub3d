b5ee453e433bdc5aa5602543fb4bd85e3586cc6f
Yann GESLIN <ygeslin@e2r2p8.42.fr>
2020-03-12 18:18:14 +0100

    jeudi12

--- cub.c
+++ cub.c
@@ -6,14 +6,17 @@
 /*   By: ygeslin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/04 11:54:16 by ygeslin           #+#    #+#             */
-/*   Updated: 2020/03/11 18:50:40 by ygeslin          ###   ########.fr       */
+/*   Updated: 2020/03/12 18:17:07 by ygeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
-int		deal_key(void *key)
+int		manage_key(int key, void *param)
 {
-	write(1, key, 1);
+	if (key == ESC)
+		ft_escape(win_ptr);
+	else if (key == A)
+		ft_a(win
 	return (1);
 }
 
@@ -25,8 +28,13 @@ int main ()
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "42");
 	mlx_pixel_put(mlx_ptr, win_ptr, 50, 50, 0xFFFFFF);
-	mlx_key_hook(win_ptr, deal_key, (void *)1);
+	mlx_key_hook(win_ptr, manage_key, (void *)0);
 	mlx_loop(mlx_ptr);
-	mlx_ptr
+	return (0);
+}
+
+int		ft_escape(t_struct s, void *win_ptr)
+{
+	write(1, "escape", 6);
 	return (0);
 }
--- cub.h
+++ cub.h
@@ -6,7 +6,7 @@
 /*   By: ygeslin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/04 11:51:38 by ygeslin           #+#    #+#             */
-/*   Updated: 2020/03/11 18:50:41 by ygeslin          ###   ########.fr       */
+/*   Updated: 2020/03/12 18:17:08 by ygeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,23 @@
 #include <unistd.h>
 #include <stdio.h>
 
+# define ESC 53
+# define W 13
+# define A 0
+# define S 1
+# define D 2
+# define UP 126
+# define DOWN 125
+# define LEFT 123
+# define RIGHT 124
 
+typedef struct s_struct
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	int		x;
+	int		y;
+}				t_struct;
 
-
+void manage_key(int key, void *param);
+int ft_escape(t_struct structure, void *win_ptr);
