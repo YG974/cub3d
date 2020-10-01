3b113650c50992291e1d7240196d779f798dd638
ygeslin <ygeslin@ygeslins-MacBook-Pro.local>
2020-10-01 19:23:38 +0200

    new gnl

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/**************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
@@ -10,20 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#define BUFFER_SIZE 80
+char	*ft_strjoin_to(char *s1, char *s2, char c);
+int		ft_next_line(int i, char *s);
+int			strchrgnl(const char *s, int c);
+
+/*		LIBC READ FUNCTION */
+
+/*		#include <unistd.h> */
+/*		ssize_t read(int fd, void *buf, size_t count); */
+/*		returns :	-1 if failed */
+/*					0 when end of file */
+/*					NB of bytes read if success */
+
+ /* GNL FUNCTION*/
+ /*	returns :	1 a line was read */
+			/*  0 end of file */
+			/*	-1 error occured */
 
 int	get_next_line(int fd, char **line)
 {
-		static char		gnl[NB_FD][BUFFER_SIZE + 1] = {{'\0'}};
-		t_gnl			s;
+		static char		*s;
+		char			buf[BUFFER_SIZE + 1];
+		char			*tmp;
+		int				ret;
 
-		s.j = 0;
-		if (fd < 0 || !line || BUFFER_SIZE < 1 || (s.i = read(fd, gnl[fd], 0)))
-				return (-1);
-		MALLCHECK((*line = (char *)malloc(sizeof(char) * 1)));
-		*line[0] = END;
-		if ((s.i = strchrgnl(gnl[fd], ENDL)) > -1)
+		if (fd < 0 || !line || BUFFER_SIZE < 1 || (ret = read(fd, buf, 0)))
+			return (-1);
+//if static buffer contains a \n
+// 1- write until \n in *line 
+// 2- move from \n to \0 at the beginning of the static buffer
+// 3- return 1
+		if ((strchrgnl(s, '\n')) > -1)
+			return (ft_next_line(ret, &s));
+		
+
+				)
+		if (!(*line = ft_strjoin_to(s, buf, '\n')))
+			return (-1);
+		while ((ret = read(fd, buf, BUFFER_SIZE) > 0)
 		{
+			buf[ret] = '\0';
+		}
 				MALLCHECK((*line = ft_strjoin_endl(*line, gnl[fd])));
 				while (s.i + s.j < BUFFER_SIZE)
 						return (ft_next_line(s.i, fd, &*gnl));
@@ -31,24 +59,23 @@ int	get_next_line(int fd, char **line)
 		MALLCHECK((*line = ft_strjoin(*line, gnl[fd])));
 		while ((s.ret = read(fd, gnl[fd], BUFFER_SIZE)) > 0)
 		{
-				gnl[fd][s.ret] = END;
+				gnl[fd][s.ret] = '\0';
 				MALLCHECK((*line = ft_strjoin_endl(*line, gnl[fd])));
-				if ((s.i = strchrgnl(gnl[fd], ENDL)) > -1)
+				if ((s.i = strchrgnl(gnl[fd], '\n')) > -1)
 						return (ft_next_line(s.i, fd, &*gnl));
 		}
-		gnl[fd][0] = '\0';
 		return (0);
 }
 
-int	ft_next_line(int i, int fd, char gnl[NB_FD][BUFFER_SIZE + 1])
+int	ft_next_line(int i, static char *s)
 {
 		int j;
 
 		j = 0;
 		while (i + j < BUFFER_SIZE)
 		{
-				gnl[fd][j] = gnl[fd][i + j + 1];
-				j++;
+			s[j] = s[i + j + 1];
+			j++;
 		}
 		return (1);
 }
@@ -61,7 +88,7 @@ char		*ft_strjoin_endl(char *s1, const char *s2)
 		char	*str;
 
 		len1 = (s1 == NULL ? 0 : ft_strlen(s1));
-		len2 = (strchrgnl(s2, ENDL) == -1 ? ft_strlen(s2) : strchrgnl(s2, ENDL));
+		len2 = (strchrgnl(s2, '\n') == -1 ? ft_strlen(s2) : strchrgnl(s2, '\n'));
 		i = 0;
 		if (!(str = malloc(sizeof(char) * (len1 + len2 + 1))))
 				return (NULL);
@@ -122,4 +149,33 @@ int			strchrgnl(const char *s, int c)
 		if (s[i] == c)
 				return (i);
 		return (-1);
+}
+
+char		*ft_strjoin_to(char *s1, char *s2, char c)
+{
+		int		len1;
+		int		len2;
+		int		i;
+		char	*str;
+
+		len1 = (s1 == NULL ? 0 : strchrgnl(s1, c));
+		len2 = (s2 == NULL ? 0 : strchrgnl(s2, c));
+		len1 = (len1 == -1 ? ft_strlen(s1) : len1);
+		len2 = (len2 == -1 ? ft_strlen(s2) : len2);
+		i = 0;
+		if (!(str = malloc(sizeof(char) * (len1 + len2 + 1))))
+				return (NULL);
+		while (i < len1)
+		{
+				str[i] = s1[i];
+				i++;
+		}
+		while (i < len1 + len2)
+		{
+				str[i] = s2[i - len1];
+				i++;
+		}
+		str[i] = '\0';
+		free(s1);
+		return (str);
 }
